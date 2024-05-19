fae375abffc47c5e20b48c234f145bc0e80b1623
BeeWeeChua <chua@bellwetherhousing.org>
2024-05-18 20:10:51 -0700

    finished (not tested yet)

--- script.js
+++ script.js
@@ -1,67 +1,46 @@
 function load() {
-    const forty_hours_in_min = 2400;
-    let hours_worked_in_min = parseInt(document.getElementById("hours").value * 60);
-    let clockin = document.getElementById("clockin").value.split(":");
-    let lunch = document.getElementById("lunch").value.split(":");
-    let lunch_end = lunchEnd(document.getElementById("lunch").value.split(":"));
+    const HR_MS = 60 * 60 * 1000;
 
-    let hours_left_in_min =
-        forty_hours_in_min - hours_worked_in_min - minWorkedBeforeLunch(clockin, lunch);
+    var clockInRaw = extractHM(document.getElementById("clockin").value);
+    var lunchRaw = extractHM(document.getElementById("lunch").value);
+    
+    var clockIn = new Date();
+    var lunch = new Date();
+    clockIn.setHours(clockInRaw.H, clockInRaw.M, 0, 0);
+    lunch.setHours(lunchRaw.H, lunchRaw.M, 0, 0);
 
+    var lunchEnd = lunch;
+    lunchEnd.setMinutes(lunchEnd.getMinutes() + 30);
+    document.getElementById("lunchend").textContent = lunchEnd.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
 
-    const time = new Date();
-    time.setHours(lunch_end[0]);
-    time.setMinutes(lunch_end[1] + ?);
-    console.log(time);
+    var hoursWorked_ms = parseFloat(document.getElementById("hours").value) * HR_MS + (lunch - clockIn);
+    var hoursLeft_ms = (40 * HR_MS) - hoursWorked_ms;
+    document.getElementById("hoursleft").textContent = parseFloat(hoursLeft_ms / HR_MS).toFixed(2);
 
+    var clockOut = lunchEnd;
+    var hoursLeft = parseInt(((document.getElementById("hoursleft").textContent).split("."))[0]);
+    var minutesLeft = parseFloat("." + ((document.getElementById("hoursleft").textContent).split("."))[1]) * 60;
+
+    console.log(hoursLeft);
+    console.log(minutesLeft);
+
+    clockOut.setHours(clockOut.getHours() + hoursLeft);
+    console.log(clockOut);
+
+    clockOut.setMinutes(clockOut.getMinutes() + minutesLeft);
+    console.log(clockOut);
+
+    document.getElementById("clockout").textContent = clockOut.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+
+    //clockOut.setMinutes(clockOut.getMinutes() + minutesLeft);
+    //console.log(clockOut);
 
 }
 
-function parseTime(t, add = 0) {
-    let h = parseInt(t[0]);
-    let m = parseInt(t[1]) + add;
-    while (m >= 60) {
-        h++;
-        m -= 60;
-    }
-    return [h, m];
-}
-
-function minWorkedBeforeLunch(clockin, lunch) {
-    let time_until_lunch = parseInt(lunch[0]) * 60 + parseInt(lunch[1]);
-    let time_until_clockin = parseInt(clockin[0]) * 60 + parseInt(clockin[1]);
-    return (time_until_lunch - time_until_clockin);
-}
-
-
-
-function lunchEnd(lunch) {
-    return parseTime([lunch[0], parseInt(lunch[1]) + 30]);
-
-    //return [h,m];
-
-    /*
-    let output_h = "";
-    let output_m = "";
-    let output_ampm = "";
-
-    if (lunchend_h > 12) {
-        output_h = (lunchend_h - 12).toString();
-        output_ampm = "PM";
-    } else if (lunchend_h == 0 || lunchend_h == 12) {
-        output_h = "12";
-        output_ampm = "PM";
-    } else {
-        output_h = lunchend_h.toString();
-        output_ampm = "AM";
-    }
-
-    if (lunchend_m < 10) {
-        output_m = "0" + (lunchend_m.toString());
-    } else {
-        output_m = lunchend_m.toString();
-    }
-
-    return (output_h + ":" + output_m + " " + output_ampm);
-    */
+function extractHM(timeString) {
+    var splitTime = timeString.split(":");
+    return {
+        H: parseInt(splitTime[0]),
+        M: parseInt(splitTime[1])
+    };
 }
