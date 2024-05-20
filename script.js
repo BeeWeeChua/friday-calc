6a2fc79615eb02e0cb95db48b09e0eb25d1a69f8
BeeWeeChua <chua@bellwetherhousing.org>
2024-05-20 10:27:09 -0700

    final final (debug & testing)

--- script.js
+++ script.js
@@ -9,32 +9,29 @@ function load() {
     clockIn.setHours(clockInRaw.H, clockInRaw.M, 0, 0);
     lunch.setHours(lunchRaw.H, lunchRaw.M, 0, 0);
 
-    var lunchEnd = lunch;
-    lunchEnd.setMinutes(lunchEnd.getMinutes() + 30);
+    if (lunch.getTime() < clockIn.getTime()) document.getElementById("lunch").value = document.getElementById("clockin").value;
+    if (parseFloat(document.getElementById("hours").value) >= 40) document.getElementById("hours").value = 39;
+
+    var lunchEnd = new Date();
+    lunchEnd.setTime(lunch.getTime() + 0.5 * HR_MS);
     document.getElementById("lunchend").textContent = lunchEnd.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
 
-    var hoursWorked_ms = parseFloat(document.getElementById("hours").value) * HR_MS + (lunch - clockIn);
+    var hoursWorked_ms = parseFloat(document.getElementById("hours").value) * HR_MS + (lunch.getTime() - clockIn.getTime());
     var hoursLeft_ms = (40 * HR_MS) - hoursWorked_ms;
+    if (hoursLeft_ms < 1) {
+        document.getElementById("hours").value = parseFloat(document.getElementById("hours").value) + (hoursLeft_ms / HR_MS) - 1;
+        load();
+        return;
+    }
     document.getElementById("hoursleft").textContent = parseFloat(hoursLeft_ms / HR_MS).toFixed(2);
 
-    var clockOut = lunchEnd;
+    var clockOut = new Date(lunchEnd.getTime());
     var hoursLeft = parseInt(((document.getElementById("hoursleft").textContent).split("."))[0]);
     var minutesLeft = parseFloat("." + ((document.getElementById("hoursleft").textContent).split("."))[1]) * 60;
 
-    console.log(hoursLeft);
-    console.log(minutesLeft);
-
     clockOut.setHours(clockOut.getHours() + hoursLeft);
-    console.log(clockOut);
-
     clockOut.setMinutes(clockOut.getMinutes() + minutesLeft);
-    console.log(clockOut);
-
     document.getElementById("clockout").textContent = clockOut.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-
-    //clockOut.setMinutes(clockOut.getMinutes() + minutesLeft);
-    //console.log(clockOut);
-
 }
 
 function extractHM(timeString) {
