b6c7de90558641d31216a6a48025ad16762c2701
BeeWeeChua <chua@bellwetherhousing.org>
2024-06-14 07:22:54 -0700

    Deleted >40hr verification

--- script.js
+++ script.js
@@ -9,18 +9,18 @@ function load() {
     clockIn.setHours(clockInRaw.H, clockInRaw.M, 0, 0);
     lunch.setHours(lunchRaw.H, lunchRaw.M, 0, 0);
 
-    if (lunch.getTime() < clockIn.getTime()) {
-        document.getElementById("lunch-container").style.backgroundColor = "yellow";
-        document.getElementById("lunch").value = document.getElementById("clockin").value;
-        document.getElementById("lunch").focus();
-        setTimeout(function() { document.getElementById("lunch-container").style.backgroundColor = ""; }, 500 ); 
-    }
-    if (parseFloat(document.getElementById("hours").value) >= 40) {
-        document.getElementById("hours-container").style.backgroundColor = "yellow";
-        document.getElementById("hours").value = 39;
-        document.getElementById("hours").focus();
-        setTimeout(function() { document.getElementById("hours-container").style.backgroundColor = ""; }, 500 ); 
-    }
+    // if (lunch.getTime() < clockIn.getTime()) {
+    //     document.getElementById("lunch-container").style.backgroundColor = "yellow";
+    //     document.getElementById("lunch").value = document.getElementById("clockin").value;
+    //     document.getElementById("lunch").focus();
+    //     setTimeout(function() { document.getElementById("lunch-container").style.backgroundColor = ""; }, 500 ); 
+    // }
+    // if (parseFloat(document.getElementById("hours").value) >= 40) {
+    //     document.getElementById("hours-container").style.backgroundColor = "yellow";
+    //     document.getElementById("hours").value = 39;
+    //     document.getElementById("hours").focus();
+    //     setTimeout(function() { document.getElementById("hours-container").style.backgroundColor = ""; }, 500 ); 
+    // }
 
     var lunchEnd = new Date();
     lunchEnd.setTime(lunch.getTime() + 0.5 * HR_MS);
@@ -28,14 +28,14 @@ function load() {
 
     var hoursWorked_ms = parseFloat(document.getElementById("hours").value) * HR_MS + (lunch.getTime() - clockIn.getTime());
     var hoursLeft_ms = (40 * HR_MS) - hoursWorked_ms;
-    if (hoursLeft_ms < HR_MS) {
-        document.getElementById("hours-container").style.backgroundColor = "yellow";
-        document.getElementById("hours").value = parseFloat(document.getElementById("hours").value) + (hoursLeft_ms / HR_MS) - 1;
-        document.getElementById("hours").focus();
-        setTimeout(function() { document.getElementById("hours-container").style.backgroundColor = ""; }, 500 ); 
-        load();
-        return;
-    }
+    // if (hoursLeft_ms < HR_MS) {
+    //     document.getElementById("hours-container").style.backgroundColor = "yellow";
+    //     document.getElementById("hours").value = parseFloat(document.getElementById("hours").value) + (hoursLeft_ms / HR_MS) - 1;
+    //     document.getElementById("hours").focus();
+    //     setTimeout(function() { document.getElementById("hours-container").style.backgroundColor = ""; }, 500 ); 
+    //     load();
+    //     return;
+    // }
     document.getElementById("hoursleft").textContent = parseFloat(hoursLeft_ms / HR_MS).toFixed(2);
 
     var clockOut = new Date(lunchEnd.getTime());
@@ -47,8 +47,12 @@ function load() {
     document.getElementById("clockout").textContent = clockOut.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
 
     for (let i = 0; i < document.getElementsByClassName("output").length; ++i) {
-        const foo = document.getElementsByClassName("output")[i];
-        if (isNaN(foo.textContent.charAt(0))) { foo.textContent = "??"; }
+        if (isNaN(document.getElementsByClassName("output")[i].textContent.charAt(0))) {
+                for (let j = 0; j < document.getElementsByClassName("output").length; ++j) {
+                    document.getElementsByClassName("output")[j].textContent = "??";
+                }
+                return;
+        }
     }
 }
 
